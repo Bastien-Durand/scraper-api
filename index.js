e60edecf3e9fd1f254beba9bb0e4fb2a91ea28d0
Bastien Durand <basilexpeditions@gmail.com>
2021-10-26 11:30:47 +1100

    refactored axios.get request and added several articles/sites

--- index.js
+++ index.js
@@ -6,31 +6,47 @@ const PORT = 8000;
 
 const app = express();
 
+const newspapers = [
+  {
+    name: 'thetimes',
+    address: 'https://www.thetimes.co.uk/environment/climate-change',
+  },
+  {
+    name: 'guardian',
+    address: 'https://www.theguardian.com/environment/climate-crisis',
+  },
+  {
+    name: 'telegraph',
+    address: 'https://www.telegraph.co.uk/climate-change',
+  },
+];
+
 const articles = [];
+
+newspapers.forEach((newspaper) => {
+  axios.get(newspaper.address).then((response) => {
+    const html = response.data;
+    const $ = cheerio.load(html);
+
+    $('a:contains("climate")', html).each(function () {
+      const title = $(this).text();
+      const url = $(this).attr('href');
+
+      articles.push({
+        title,
+        url,
+        source: newspaper.name,
+      });
+    });
+  });
+});
 
 app.get('/', (req, res) => {
   res.json('Welcome to my Climate Change API');
 });
 
 app.get('/news', (req, res) => {
-  axios
-    .get('https://www.theguardian.com/environment/climate-crisis')
-    .then((response) => {
-      const html = response.data;
-      const $ = cheerio.load(html);
-
-      $('a:contains("climate")', html).each(function () {
-        const title = $(this).text();
-        const url = $(this).attr('href');
-
-        articles.push({
-          title,
-          url,
-        });
-      });
-      res.json(articles);
-    })
-    .catch((err) => console.log(err));
+  res.json(articles);
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
